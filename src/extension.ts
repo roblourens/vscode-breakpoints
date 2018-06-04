1e0d8f4e919d041874d357b9f14c22ba32a43b1d
Rob Lourens <roblourens@gmail.com>
2018-06-03 19:04:31 -0700

    Export working

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,80 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as path from 'path';
+import * as fs from 'fs';
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import { promisify } from 'es6-promisify';
+const writeFileP: (filename: string, data: any) => Promise<void> = promisify(fs.writeFile);
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-breakpoints" is now active!');
+type Maybe<T> = T | null | undefined;
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+// Things I need:
+// - command to export current breakpoints
+// - command to import breakpoints from some file
+//      - How to select the file?
+//      - ** Some file at well-known location, like .vscode/breakpoints.json
+//          - Multi-root workspaces?
+//          - Saved as a setting?
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+type CodeBreakpoint = vscode.SourceBreakpoint | vscode.FunctionBreakpoint;
+interface SerializedBreakpoint {
+    enabled: boolean;
+    condition?: string;
+    hitCondition?: string;
+    logMessage?: string;
 
-    context.subscriptions.push(disposable);
+    // SourceBreakpoint
+    path?: string;
+    position?: { line: number, character: number };
+
+    // FunctionBreakpoint
+    functionName?: string;
 }
 
-// this method is called when your extension is deactivated
+interface IExportedBreakpoints {
+    breakpoints: SerializedBreakpoint[];
+}
+
+const BP_EXPORT_PATH_IN_WORKSPACE = '.vscode/breakpoints.json';
+
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(
+        vscode.commands.registerCommand('vscode-breakpoints.exportBreakpoints', async () => {
+            const exportedBps: IExportedBreakpoints = {
+                breakpoints: vscode.debug.breakpoints.map(bp => serializeBreakpoint(bp as CodeBreakpoint))
+            };
+
+            // const folder = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0];
+            let bpFileLocation: Maybe<string>;
+            // if (folder && folder.uri.scheme === 'file') {
+            //     bpFileLocation = path.join(folder.uri.fsPath, BP_EXPORT_PATH_IN_WORKSPACE);
+            // } else {
+                const saveUri = await vscode.window.showSaveDialog({ filters: { 'JSON': ['json'] } });
+                bpFileLocation = saveUri && saveUri.fsPath;
+            // }
+
+            if (bpFileLocation) {
+                await writeFileP(bpFileLocation, JSON.stringify(exportedBps, undefined, '  '));
+            }
+        }));
+}
+
+function serializeBreakpoint(bp: CodeBreakpoint): SerializedBreakpoint {
+    return {
+        enabled: bp.enabled,
+        condition: bp.condition,
+        hitCondition: bp.hitCondition,
+        logMessage: bp.logMessage,
+        ...(bp instanceof vscode.SourceBreakpoint ?
+            {
+                path: bp.location.uri.fsPath,
+                position: bp.location.range.start
+            } :
+            {
+                functionName: bp.functionName
+            })
+    };
+}
+
 export function deactivate() {
 }
